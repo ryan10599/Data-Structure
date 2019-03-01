ec51d51e6cfcc081e94edf7054688cd9aca1d166
Ryan <thanapol.ryan@mail.kmutt.ac.th>
2019-03-01 12:26:04 +0700

    [Add]

---
+++ TestHashSet/src/WordFrequency/TestWordFrequency.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WordFrequency;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author ryan.
+ */
+public class TestWordFrequency {
+    public static void main(String[] args) {
+        FrequencyCounter fc = new FrequencyCounter();
+        try{
+            fc.process("/Users/ryan./Documents/GitHub/Data-Structure/TestHashSet/src/WordFrequency/data.txt");
+            System.out.println(fc);
+        }catch(FileNotFoundException ex){
+            System.out.println(ex);
+        }
+    }
+}
