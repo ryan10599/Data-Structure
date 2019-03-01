6d078341c9025551ddc1d583316137ef3f013e33
Ryan <thanapol.ryan@mail.kmutt.ac.th>
2019-03-01 10:43:52 +0700

    [Update] class NewStudent

--- TestHashSet/src/testhashset/TestHashSet.java
+++ TestHashSet/src/testhashset/TestHashSet.java
@@ -5,6 +5,9 @@
  */
 package testhashset;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author ryan.
@@ -15,7 +18,52 @@ public class TestHashSet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Set<String> s = new HashSet<>();
+        s.add("Cat");
+        s.add("Dog");
+        s.add("Bird");
+        s.add("Duck");
+        
+        String x = new String("Cat");
+        
+        System.out.println("code: "+"Cat".hashCode());
+        System.out.println("code: "+x.hashCode());
+        
+        System.out.println(s.contains("Dog"));
+        
     }
+    
+}
+
+class NewStudent{
+    private int id;
+    private String name;
+
+    public NewStudent(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "NewStudent{" + "id=" + id + ", name=" + name + '}';
+    }
+    
     
 }
