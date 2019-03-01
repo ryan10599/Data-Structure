08d9f7991b7101e87ae833c534646d8f18239000
Ryan <thanapol.ryan@mail.kmutt.ac.th>
2019-03-01 10:57:16 +0700

    [Update] เพิ่ม method testhashcode

--- TestHashSet/src/testhashset/TestHashSet.java
+++ TestHashSet/src/testhashset/TestHashSet.java
@@ -18,18 +18,36 @@ public class TestHashSet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Set<String> s = new HashSet<>();
-        s.add("Cat");
-        s.add("Dog");
-        s.add("Bird");
-        s.add("Duck");
+//        Set<String> s = new HashSet<>();
+//        s.add("Cat");
+//        s.add("Dog");
+//        s.add("Bird");
+//        s.add("Duck");
+//        
+//        String x = new String("Cat");
+//        
+//        System.out.println("code: "+"Cat".hashCode());
+//        System.out.println("code: "+x.hashCode());
+//        
+//        System.out.println(s.contains("Dog"));
+
+    testHashCode();
         
-        String x = new String("Cat");
+    }
+    
+    static void testHashCode(){
+        Set<NewStudent> st = new HashSet();
+        st.add(new NewStudent(1001,"Somchai"));
+        st.add(new NewStudent(1002,"Somsri"));
+        st.add(new NewStudent(1003,"Somkiet"));
+        NewStudent ns = new NewStudent(1002,"Somsri");
+        System.out.println(st);
+        System.out.println(ns);
+        System.out.println(st.contains(ns));
         
-        System.out.println("code: "+"Cat".hashCode());
-        System.out.println("code: "+x.hashCode());
-        
-        System.out.println(s.contains("Dog"));
+        NewStudent ns2 = new NewStudent(1002,"Somsri");
+        System.out.println("hashcode of ns: "+ns.hashCode());
+        System.out.println("hashcode of ns2: "+ns2.hashCode());
         
     }
     
