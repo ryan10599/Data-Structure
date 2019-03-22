efe61e1c61ff61d978735330b9dcea212ac2f383
Ryan <thanapol.ryan@mail.kmutt.ac.th>
2019-03-22 11:55:31 +0700

    [Update] Use anonymous

--- TestPriorityQueue/src/testpriorityqueue/TestPriorityQueue.java
+++ TestPriorityQueue/src/testpriorityqueue/TestPriorityQueue.java
@@ -19,7 +19,12 @@ public class TestPriorityQueue {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue(20, new MaxHeapComparator());
+        PriorityQueue<Integer> pq = new PriorityQueue(20, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
         Random rd = new Random();
         for (int i = 0; i < 20; i++) {
             int x = rd.nextInt(99);
